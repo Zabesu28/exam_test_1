1abfca3b03d6abe3fa6f872dce7777c74b0d49bb
RomainZabeth <112702309+Zabesu28@users.noreply.github.com>
2025-11-17 16:14:23 +0100

    dqsdq

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Comment gérer différentes URLs pour le développement, la pré-production et la production ?
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + '/api',
+  baseURL: (process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'https://backend-k9wz.onrender.com') + '/api',
 });
 
 export default api;
